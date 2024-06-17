be53a681c28a63ede62373e2d754da70f384fbb0
Murari Sai Karthik <saikarthikmurari488@gmail.com>
2024-06-17 13:30:07 +0530

    Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h2>My Projects</h2>
+      <div className="project-list">
+        {/* Add your projects here */}
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
