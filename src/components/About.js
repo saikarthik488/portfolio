c4950e1f4326e5bedc8cd5f250a3ff57f32255be
Murari Sai Karthik <saikarthikmurari488@gmail.com>
2024-06-17 13:29:38 +0530

    About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>Information about your career and journey.</p>
+    </div>
+  );
+}
+
+export default About;
