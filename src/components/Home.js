1f04d8f4b2fecb0901368d8f4889545ed1b115ad
Murari Sai Karthik <saikarthikmurari488@gmail.com>
2024-06-17 13:29:16 +0530

    Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>I am a creative developer.</p>
+    </div>
+  );
+}
+
+export default Home;
